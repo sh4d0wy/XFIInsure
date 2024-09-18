04fc88e72c4e44f18d130aa665ef2c6150ccb585
sh4d0wy <sakshambhugra8@gmail.com>
2024-09-18 23:49:58 +0530

    Chore:Final commit due to lack of testnet tokens

--- contracts/interfaces/IToken.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-interface IToken is IERC20{
-    function mint(address _to, uint256 _amount) external;
-    function burn(address from ,uint256 _amount) external;
-}
